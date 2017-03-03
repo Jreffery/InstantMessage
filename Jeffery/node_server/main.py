093a3b9c9420e49ac3b586983e508bf9d0500399
RuanRuan <1933458296@qq.com>
2017-03-03 12:29:49 +0800

    同步编码

--- Jeffery/node_server/main.py
+++ Jeffery/node_server/main.py
@@ -3,12 +3,13 @@ Created on 2016/10/6
 
 @author: Jeffery
 '''
+#encoding: utf-8
 
 from twisted.internet import reactor
 from cc.appweb.dims import register, server
 
 if __name__ == '__main__':
+    # ¼àÌýÕß
     reactor.connectTCP('localhost',8000, register.RegisterFactory(reactor))
-    # è¿›å…¥äº‹ä»¶å¾ªçŽ¯
     reactor.run()
     
