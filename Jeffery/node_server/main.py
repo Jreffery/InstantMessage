69436f85b4a68b4558488a4ec2debfea2ab99190
Jreffery <645469584@qq.com>
2017-03-03 12:41:51 +0800

    同步编码

--- Jeffery/node_server/main.py
+++ Jeffery/node_server/main.py
@@ -1,4 +1,4 @@
-'''
+ï»¿'''
 Created on 2016/10/6
 
 @author: Jeffery
@@ -8,7 +8,7 @@ from twisted.internet import reactor
 from cc.appweb.dims import register, server
 
 if __name__ == '__main__':
-    # ²âÊÔ±àÂë
+    # é‡æ–°æµ‹è¯•ç¼–ç 
     reactor.connectTCP('localhost',8000, register.RegisterFactory(reactor))
     reactor.run()
     
